ab79513fdc59bb99ddc4acab8478f8dcb672a39b
Pavel Durov <pav3ldurov@gmail.com>
2022-10-16 16:03:03 +0100

    chore: publications styling

---
+++ src/data/publications.ts
@@ -0,0 +1,45 @@
+const publications = [
+  {
+    title: 'PyPy,Python and performance benchmarking',
+    url: 'https://iamkimchi.hashnode.dev/pypypython-and-performance-benchmarking'
+  },
+
+  {
+    title: 'Linux VNC server & macOS VNC client.',
+    url: 'https://iamkimchi.hashnode.dev/linux-vnc-server-macos-vnc-client'
+
+  }, {
+    title: 'Introduction to numerical bases.',
+    url: 'https://www.codeproject.com/Articles/1034345/Introduction-to-numerical-bases'
+  }, {
+
+    title: 'Accessing Internal Data on Android Device',
+    url: 'https://www.codeproject.com/Articles/825304/Accessing-Internal-Data-on-Android-Device'
+  }, {
+    title: 'An Introduction to Android NDK',
+    url: 'https://www.codeproject.com/Articles/824711/An-Introduction-to-Android-NDK'
+  }, {
+    title: 'Android Basic Game Loop',
+    url: 'https://www.codeproject.com/Articles/827608/Android-Basic-Game-Loop'
+  }, {
+    title: 'Android dynamic Sqlite database management.',
+
+    url: 'https://www.codeproject.com/Articles/829272/Android-dynamic-Sqlite-database-management-v'
+  }, {
+
+    title: 'Introduction to Mvvmcross Platform.',
+    url: 'https://www.codeproject.com/Articles/863695/Introduction-to-Mvvmcross-Platform'
+  }, {
+
+    title: 'Android and .NET Encryption.',
+    url: 'https://www.codeproject.com/Articles/826457/Android-and-NET-Encryption'
+  }, {
+    title: 'Uploading large files from Android to ASP.NET',
+    url: 'https://www.codeproject.com/Articles/829950/Uploading-large-files-from-Android-to-ASP-NET'
+
+  }, {
+    title: 'Hosted Javascript-WinRT duplex communication',
+    url: 'https://www.codeproject.com/Articles/1008161/Hosted-Javascript-WinRT-duplex-communication'
+  }
+]
+export default publications
--- src/routes/publications.tsx
+++ src/routes/publications.tsx
@@ -1,116 +1,34 @@
+import publications from '../data/publications'
+
+// function randomNumber (min: number, max: number): number {
+//   return Math.random() * (max - min) + min
+// }
+
+const bgs = ['bg-primary', 'bg-success', 'bg-error', 'bg-danger', 'bg-warning', 'bg-info']
+
+function getClass (i: number): string {
+  const bg = bgs[i % bgs.length]
+  // bgs[randomNumber(0, bgs.length)]
+  return `card text-white bg-secondary mb-3 ${bg}`
+}
+
 export function Publications (): JSX.Element {
   return (
     <>
-      <section className="container">
+      <section className="container col-lg-4">
         <h1>Publications</h1>
-        <table className="table table-hover">
-          <tr>
-            <td>
-              <p>
-                PyPy,Python and performance benchmarking
-                https://iamkimchi.hashnode.dev/pypypython-and-performance-benchmarking
-              </p>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <p>
-                Linux VNC server & macOS VNC client.
-                https://iamkimchi.hashnode.dev/linux-vnc-server-macos-vnc-client
-              </p>
-            </td>
-          </tr>
 
-          <tr>
-            <td>
-              <p>
-                Basics of numeric bases by examples on four bases - decimal,
-                binary, hex and octal.
-                https://www.codeproject.com/Articles/1034345/Introduction-to-numerical-bases
-              </p>
-            </td>
-          </tr>
+          {
+            publications.map((publication, idx) => (
+              <div key={idx} className={getClass(idx)}>
+                <div className="card-header">{publication.title}</div>
+                <div className="card-body">
+                  <a href={publication.url}>{publication.url}</a>
+                </div>
+              </div>
+            ))
+          }
 
-          <tr>
-            <td>
-              <p>
-                This article will explain how to explore Android internal data
-                from Eclipse DDMS perspective using adb and Linux commands.
-                https://www.codeproject.com/Articles/825304/Accessing-Internal-Data-on-Android-Device
-              </p>
-            </td>
-          </tr>
-
-          <tr>
-            <td>
-              <p>
-                Basic NDK implementation using Hello World application
-                https://www.codeproject.com/Articles/824711/An-Introduction-to-Android-NDK
-              </p>
-            </td>
-          </tr>
-
-          <tr>
-            <td>
-              <p>
-                This article is explaining how to implement a basic game
-                fundamentals on Android platform.
-                https://www.codeproject.com/Articles/827608/Android-Basic-Game-Loop
-              </p>
-            </td>
-          </tr>
-
-          <tr>
-            <td>
-              <p>
-                Article about dynamic Sqlite database management on Android
-                platform using Java class reflection.
-                https://www.codeproject.com/Articles/829272/Android-dynamic-Sqlite-database-management-v
-              </p>
-            </td>
-          </tr>
-
-          <tr>
-            <td>
-              <p>
-                Brief overview on the basic concepts of mvvmcross platform. We
-                will explore the mvvmcross project architecture and some
-                mvvmcross classes.
-                https://www.codeproject.com/Articles/863695/Introduction-to-Mvvmcross-Platform
-              </p>
-            </td>
-          </tr>
-
-          <tr>
-            <td>
-              <p>
-                Basic encryption/decryption on Android and on .NET platforms,
-                with focus on passing encrypted data from Android to .NET.
-                https://www.codeproject.com/Articles/826457/Android-and-NET-Encryption
-              </p>
-            </td>
-          </tr>
-
-          <tr>
-            <td>
-              <p>
-                Uploading large files from Android to ASP.NET
-                https://www.codeproject.com/Articles/829950/Uploading-large-files-from-Android-to-ASP-NET
-              </p>
-            </td>
-          </tr>
-
-          <tr>
-            <td>
-              <p>
-                I explain how to create a local WebView hosted Html/Javascript
-                application on Winrt Platform and how to establish the
-                communication between both sides.
-                https://www.codeproject.com/Articles/1008161/Hosted-Javascript-WinRT-duplex-communication
-              </p>
-            </td>
-          </tr>
-        </table>
       </section>
     </>
   )

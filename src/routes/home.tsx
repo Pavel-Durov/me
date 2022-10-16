b7233cdff15e17e3ae665602cea7ca8466886d96
Pavel Durov <pav3ldurov@gmail.com>
2022-10-16 14:34:30 +0100

    chore: identities iteration

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,48 +1,41 @@
 import Profile from '../assets/profile.png'
+import identities from '../data/identities'
 
 export function Home (): JSX.Element {
   return (
-
     <div className="container tab-content">
-        <div className="tab-pane active" id="home" role="tabpanel" aria-labelledby="home-tab">
-          <section className="container m-5">
+      <div
+        className="tab-pane active"
+        id="home"
+        role="tabpanel"
+        aria-labelledby="home-tab"
+      >
+        <section className="container m-5">
           <img src={Profile} width="100" className="m-5" />
 
-            <p>
-              I have always been curious and fascinated by the world inner workings and design.
-              I am a software engineer with experience developing and leading projects from scratch to enterprise products.
-              My main technical interests are programming languages structure, distributed systems, Linux and open source.
-            </p>
-          </section>
+          <p>
+            I have always been curious and fascinated by the world inner
+            workings and design. I am a software engineer with experience
+            developing and leading projects from scratch to enterprise products.
+            My main technical interests are programming languages structure,
+            distributed systems, Linux and open source.
+          </p>
+        </section>
 
-          <br />
+        <br />
 
-          <section>
-            <h2>Identities</h2>
-            <ul className="list-group">
-              <li className="list-group-item">
-                <a href="https://github.com/Pavel-Durov">Github</a>
-              </li>
-              <li className="list-group-item">
-                <a href="https://twitter.com/pav3ldurov">Twitter</a>
-              </li>
-              <li className="list-group-item">
-                <a href="https://www.linkedin.com/in/paveldurov/">LinkedIn</a>
-              </li>
-              <li className="list-group-item">
-                <a href="https://stackoverflow.com/users/5321395/pavel-durov">Stackoverflow</a>
-              </li>
-              <li className="list-group-item">
-                <a href="https://iamkimchi.hashnode.dev/">Hashnote</a>
-              </li>
-              <li className="list-group-item">
-                <a href="https://medium.com/@iamkimchi">Medium</a>
-              </li>
-            </ul>
-          </section>
+        <section>
+          <h2>Identities</h2>
 
-        </div>
+          <ul className="list-group">
+            {identities.map((identity, idx) => (
+              <li key={idx} className="list-group-item">
+                <a href={identity.url}>{identity.name}</a>
+              </li>
+            ))}
+          </ul>
+        </section>
       </div>
-
+    </div>
   )
 }

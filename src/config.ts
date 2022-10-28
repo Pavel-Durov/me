102ddc44374b0485f34e1e88b7b3e016e25ccdb4
Pavel Durov <pav3ldurov@gmail.com>
2022-10-28 16:17:59 +0100

    chore: moved loader to app component

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,2 @@
-export const BROWSER_SESSION_BASED_LOADING_HOME = true
+export const BROWSER_SESSION_BASED_LOADING_HOME = false
+export const LOADING_SPEED_FACTOR = 10
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,47 +1,13 @@
 import React from 'react'
 import Profile from 'assets/profile.png'
-import { BROWSER_SESSION_BASED_LOADING_HOME } from 'config'
 import identities from 'data/identities'
 
-interface HomeState {
-  loadProgress: number
-}
-
-export class Home extends React.Component<{}, HomeState> {
-  constructor (props: {}) {
-    super(props)
-    let loadProgress = 0
-    if (BROWSER_SESSION_BASED_LOADING_HOME) {
-      const progress = sessionStorage.getItem('homeLoadProgress')
-      loadProgress = progress == null ? 0 : parseInt(progress, 10)
-    }
-    this.state = {
-      loadProgress
-    }
-  }
-
-  componentDidMount (): void {
-    if (this.state.loadProgress !== 100) {
-      for (let i = 1; i <= 100; i++) {
-        setTimeout(() => {
-          this.setState({ loadProgress: i })
-          sessionStorage.setItem('homeLoadProgress', i.toString())
-        }, i * 50)
-      }
-    }
-  }
-
+export class Home extends React.Component {
   render (): React.ReactNode {
     return (
       <div className="container tab-content">
-        <div hidden={this.state.loadProgress === 100} style={{ margin: '50% 0 0 0 ' }}>
-          <h5 className="mb-1">Loading...</h5>
-          <div className="progress">
-            <div className="progress-bar bg-warning" role="progressbar" style={{ width: `${this.state.loadProgress}%` }}></div>
-          </div>
-          <div></div>
-        </div>
-        <section className="content" hidden={this.state.loadProgress !== 100}>
+
+        <section className="content">
           <section className="container m-1">
             <img src={Profile} width="100" className="m-5" />
             <p>

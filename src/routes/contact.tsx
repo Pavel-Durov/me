a6265b9689cd825137d7386f777d3fb3cfe4798c
Pavel Durov <pav3ldurov@gmail.com>
2022-10-28 09:38:44 +0100

    chore: loading bar

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -4,10 +4,10 @@ export function Contact (): JSX.Element {
       <h1>Contact Me</h1>
       <ul className="list-group">
         <li className="list-group-item">
-          <a href="https://keys.openpgp.org/search?q=EE415FF7FDA3888D1D9FB546006673FCAA91C9E4">PGP</a>
+          <a href="https://keys.openpgp.org/search?q=EE415FF7FDA3888D1D9FB546006673FCAA91C9E4">PGP Key</a>
         </li>
         <li className="list-group-item">
-          <a href="iamkimchi@proton.me">Email</a>
+          <a href="mailto:iamkimchi@proton.me">email: iamkimchi@proton.me</a>
         </li>
       </ul>
     </section>
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -2,22 +2,50 @@ import React from 'react'
 import Profile from '../assets/profile.png'
 import identities from '../data/identities'
 
-export class Home extends React.Component {
+interface HomeState {
+  loadProgress: number
+}
+
+export class Home extends React.Component<{}, HomeState> {
+  constructor (props: {}) {
+    super(props)
+    const progress = sessionStorage.getItem('homeLoadProgress')
+    const loadProgress = progress == null ? 0 : parseInt(progress, 10)
+    this.state = {
+      loadProgress
+    }
+  }
+
+  componentDidMount (): void {
+    if (this.state.loadProgress !== 100) {
+      for (let i = 1; i <= 100; i++) {
+        setTimeout(() => {
+          this.setState({ loadProgress: i })
+          sessionStorage.setItem('homeLoadProgress', i.toString())
+        }, i * 50)
+      }
+    }
+  }
+
   render (): React.ReactNode {
     return (
       <div className="container tab-content">
-        <div className="tab-pane active" id="home" role="tabpanel" aria-labelledby="home-tab">
+        <div className="m-4" hidden={this.state.loadProgress === 100}>
+          <h5 className="mb-1">Loading...</h5>
+          <div className="progress">
+            <div className="progress-bar bg-warning" role="progressbar" style={{ width: `${this.state.loadProgress}%` }}></div>
+          </div>
+          <div></div>
+        </div>
+        <section className="content" hidden={this.state.loadProgress !== 100}>
           <section className="container m-1">
             <img src={Profile} width="100" className="m-5" />
-
             <p>
               I have always been curious and fascinated by the world inner workings and design. I am a software engineer with experience developing and leading projects from
               scratch to enterprise products. My main technical interests are programming languages, distributed systems, Linux and open source.
             </p>
           </section>
-
           <br />
-
           <section>
             <h2>Identities</h2>
 
@@ -29,7 +57,7 @@ export class Home extends React.Component {
               ))}
             </ul>
           </section>
-        </div>
+        </section>
       </div>
     )
   }

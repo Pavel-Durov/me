01bff8acb0d49789202a43c1470ae1c9338d3e8d
Pavel Durov <pav3ldurov@gmail.com>
2022-10-29 20:46:01 +0100

    chore: styling on contact and talks pages

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -2,29 +2,26 @@ import contact from 'data/contact'
 
 export function Contact (): JSX.Element {
   return (
+    <section>
+      <h1>{contact.title}</h1>
+      <div className="jumbotron">
+        <p>{contact.contactMessage}</p>
+        <p className="lead">
+          <a className="btn btn-primary btn-lg" href={'mailto:' + contact.email} role="button">
+            Send me an email
+          </a>
+        </p>
+      </div>
 
-    <div>
-        <div className="page-header">
-          <h1 id="containers">{contact.title}</h1>
-        </div>
-        <div className="bs-component">
-          <div className="jumbotron">
-            <p>{contact.contactMessage}</p>
-            <p className="lead">
-              <a className="btn btn-primary btn-lg" href={'mailto:' + contact.email} role="button">
-                Send me an email
-              </a>
-            </p>
+        <section>
+          <div className="m-2">
+            <a className="btn btn-large btn-primary" href={contact.pgpPublicKey}>
+              Download PGP Key
+            </a>
           </div>
-        </div>
-      <section>
-        <div className="m-2">
-          <a className="btn btn-large btn-primary" href={contact.pgpPublicKey}>Download PGP Key</a>
-        </div>
-        <code>
-          {contact.pgpKey}
-        </code>
-      </section>
-    </div>
+          <code>{contact.pgpKey}</code>
+        </section>
+
+    </section>
   )
 }

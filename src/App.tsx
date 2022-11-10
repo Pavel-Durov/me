23c3ce6ed03ae6efa7b21ae2a503e92e92b6d641
Pavel Durov <pav3ldurov@gmail.com>
2022-11-10 20:25:26 +0000

    chore: app width

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,8 @@ export default class App extends React.Component<{}, HomeState> {
         </div>
 
         <div className="App bootstra-enable-cursor container col-md-8 col-sm-11 text-left"
-        style={{ marginTop: '30px' }}hidden={this.state.loadProgress !== 100} >
+
+        style={{ marginTop: '30px', maxWidth: 800 }}hidden={this.state.loadProgress !== 100} >
           <NavBar />
           <RouterProvider router={router} />
           <footer className='fixed-bottom'>

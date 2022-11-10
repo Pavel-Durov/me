ad1d6570893b8c3677fdedc38b6148c0d9164ee6
Pavel Durov <pav3ldurov@gmail.com>
2022-11-10 20:25:35 +0000

    chore: nav-bar text

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import Profile from 'assets/head_spin_yellow_bg.gif'
 export function NavBar (): JSX.Element {
   return (
     <>
-      <div className="navbar navbar-expand-sm fixed-top navbar-dark bg-primary text-center">
+      <div className="navbar navbar-expand-sm fixed-top navbar-dark bg-primary">
         <img src={Profile} style={{ margin: '1px', width: 30, padding: '5px' }}/>
 
         <mark>[PAVEL DUROV]</mark>

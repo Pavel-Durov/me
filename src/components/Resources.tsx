de801f3b10e32eb2033dfed515cc5b770df182d7
Pavel Durov <pav3ldurov@gmail.com>
2022-11-19 16:17:21 +0000

    feat: resources

--- src/components/Resources.tsx
+++ src/components/Resources.tsx
@@ -1,22 +1,84 @@
-import { getBgClass } from 'components/Utils'
 import { Resources as Model } from 'model'
+import React from 'react'
 
-export function Resources ({ references }: { references: Model[] }): JSX.Element {
-  return (
-    <>
-      <section>
-        <h1>Resources</h1>
-        {references.map((reference, idx) => (
-          <div key={idx} className={getBgClass(idx)}>
-            <div className="card-header">
-              <a href={reference.url}>{reference.title}</a>
-            </div>
-            <div className="card-body">
-              <p>{reference.description}</p>
-            </div>
+export class Resources extends React.Component<{ references: Model[] }, { references: Model[], items: Model[], selectedItem?: Model }> {
+  constructor (props: { references: Model[] }) {
+    super(props)
+    this.state = {
+      items: props.references,
+      references: props.references,
+      selectedItem: props.references[0]
+    }
+  }
+
+  itemSelected (idx: number): void {
+    this.setState({
+      selectedItem: this.state.items[idx]
+    })
+  }
+
+  onTextChange (text: string | null): void {
+    this.setState({ selectedItem: undefined })
+    if (text === null) {
+      this.setState({
+        items: this.props.references
+      })
+    } else {
+      const filtered = this.props.references.filter((item) => {
+        // const set = new Set()
+        if (item.title.toLowerCase().includes(text.toLowerCase())) {
+          console.log(item)
+          return true
+        }
+        if (item.tags.join(',').includes(text.toLowerCase())) {
+          console.log(item)
+          return true
+        }
+        return false
+      })
+      this.setState({
+        items: filtered
+      })
+    }
+  }
+
+  render (): JSX.Element {
+    return (
+      <section className='kimchi-box'>
+        <div >
+          <span>Search:</span>
+          <input type="text" className="form-control" onChange={(e) => this.onTextChange(e.target.value)}/>
+        </div>
+        <br/>
+        <section style={{ display: 'flex' }}>
+          <div className="kimchi-list" style={{ width: '50%' }}>
+            {this.state.items.map((item, idx) => (
+              <div key={idx} onClick={(e) => this.itemSelected(idx)}>
+                <p>
+                <code>{item.title}</code>
+                </p>
+                <p>
+                  [{item?.tags.join(', ')}]
+                </p>
+              </div>
+            ))}
           </div>
-        ))}
+          <div className="kimchi-list" style={{ width: '50%', padding: 5 }}>
+            {
+              (this.state.selectedItem != null)
+                ? <div>
+                <p>
+                  <a href={this.state.selectedItem?.url}>link</a>
+                </p>
+                <p>{this.state.selectedItem?.description}</p>
+              </div>
+                : <div/>
+            }
+
+          </div>
+          {/* <td style={{ verticalAlign: 'text-top' }}>Show details here</td> */}
+        </section>
       </section>
-    </>
-  )
+    )
+  }
 }

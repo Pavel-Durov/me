8a534c5ec29ee856858734f7fbb7c7c3c5025801
Pavel Durov <pav3ldurov@gmail.com>
2022-12-11 11:44:44 +0000

    feat: plc publication

--- src/data/publications.ts
+++ src/data/publications.ts
@@ -2,45 +2,58 @@ import { Publication } from 'model'
 
 const publications: Publication[] = [
   {
+    title: 'Programming Languages Composition',
+    description:
+      'There is a variety of Programming Languages (PL) today, each with its speciality and features. But still, traditional software is written using a single programming language. We have tools and processes that allow language compositions, but these tools are cumbersome, slow and complex. This article will overview the Programming Language Composition (PLC) problem space and the applied composition techniques.',
+    url: 'https://blog.p3ld3v.dev/programming-languages-composition',
+    date: '2022-12-10'
+  },
+  {
     title: 'Cross Platform CLI application with GO and Cobra',
-    description: 'This article will cover the process and the involved components of building a Command-Line Interface (CLI)[1] application using the GO programming language. We will cover the required libraries, directory structure, configuration files, testing, and Cross-Platform build process',
+    description:
+      'This article will cover the process and the involved components of building a Command-Line Interface (CLI)[1] application using the GO programming language. We will cover the required libraries, directory structure, configuration files, testing, and Cross-Platform build process',
     url: 'https://blog.p3ld3v.dev/cross-platform-cli-application-with-go-and-cobra',
     date: '2022-12-02'
   },
   {
     title: 'Progressive Delivery - Application deployment strategies: configurable and gradual deployments with minimised downtime',
-    description: 'This article will cover Progressive Delivery techniques such as Feature Flags, A/B Testing, Ring Deployment, Dark Launching, Blue/Green and Canary deployments with high-level illustrations and a general concept overview.',
+    description:
+      'This article will cover Progressive Delivery techniques such as Feature Flags, A/B Testing, Ring Deployment, Dark Launching, Blue/Green and Canary deployments with high-level illustrations and a general concept overview.',
     url: 'https://blog.p3ld3v.dev/progressive-delivery',
     date: '2022-11-21'
   },
   {
     title: 'Hexagonal Architecture/Ports and Adapters, DDD, DI and IoC',
-    description: 'In this article, I will overview the concept of Hexagonal Architecture (HA), the problem it addresses, its benefits, implications and some practical examples illustrated via code and diagrams.There\'re many sources on this topic. My source is not the most detailed one, but my intention is to simplify the idea as much as I can and break it down to its core concepts and explain its jargon.',
+    description:
+      "In this article, I will overview the concept of Hexagonal Architecture (HA), the problem it addresses, its benefits, implications and some practical examples illustrated via code and diagrams.There're many sources on this topic. My source is not the most detailed one, but my intention is to simplify the idea as much as I can and break it down to its core concepts and explain its jargon.",
     url: 'https://blog.p3ld3v.dev/hexagonal-architectureports-and-adapters-ddd-di-and-ioc',
     date: '2022-11-14'
   },
   {
     title: 'Graceful Termination in K8S - SIGTERM and UNIX processes',
-    description: 'In this article, we\'re going to cover UNIX process signals and SIGTERM signals in particular. We will cover how to handle them with practical examples using Node, TypeScript, Docker and Kind local cluster.',
+    description:
+      "In this article, we're going to cover UNIX process signals and SIGTERM signals in particular. We will cover how to handle them with practical examples using Node, TypeScript, Docker and Kind local cluster.",
     url: 'https://blog.p3ld3v.dev/graceful-termination-in-k8s-sigterm-and-unix-processes',
     date: '2022-11-07'
   },
   {
     title: 'macOS FS vs Linux FS - case sensitivity',
-    description: 'In this article, I will overview the case-sensitivity difference between macOS default filesystem (FS) and Linux filesystem with some practical examples and gotchas',
+    description:
+      'In this article, I will overview the case-sensitivity difference between macOS default filesystem (FS) and Linux filesystem with some practical examples and gotchas',
     url: 'https://blog.p3ld3v.dev/macos-fs-vs-linux-fs-case-sensitivity',
     date: '2022-10-31'
   },
   {
     title: 'Python logging module quirks',
     description:
-    'The Python logger module might resemble familiar tools, but it has its specifics. In this article, I would like to cover some quirks that I found along the way while I was working recently on a python project',
+      'The Python logger module might resemble familiar tools, but it has its specifics. In this article, I would like to cover some quirks that I found along the way while I was working recently on a python project',
     url: 'https://blog.p3ld3v.dev/python-logging-module-quirks',
     date: '2022-10-20'
   },
   {
     title: 'Db data migration using K8S jobs Helm. Or how I learned to let go & love Helm',
-    description: 'This article will cover the data migration process between two data stores, the evaluation of possible alternatives, and final outcome. We will cover the difference between sequential, asynchronous, multi-threaded, and distributed applications. And finally, we will overview the practical application of Helm and K8S jobs for data migration tasks',
+    description:
+      'This article will cover the data migration process between two data stores, the evaluation of possible alternatives, and final outcome. We will cover the difference between sequential, asynchronous, multi-threaded, and distributed applications. And finally, we will overview the practical application of Helm and K8S jobs for data migration tasks',
     url: 'https://blog.p3ld3v.dev/db-data-migration-using-k8s-jobs-helm',
     date: '2022-10-29'
   },

8296d66df2440474d595f40dd50cc443cce53e5c
Pavel Durov <pav3ldurov@gmail.com>
2022-12-22 18:58:32 +0200

    feat: home prevent-select

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -10,7 +10,7 @@ export class Home extends React.Component {
           <MovingHead/>
         </div>
         <p>You have landed at the home page of Pavel Durov.</p>
-        <p className='non-editable'>
+        <p className='prevent-select'>
           I am a human, genuinely interested in technology, philosophy, music, gardening, running, bouldering and planet earth. I live with my wife and two cats in London, UK. My
           technical interests are Programming Languages, Open Source, Linux, Distributed Systems and Software Architecture. But you will probably find me doing other stuff most of
           the time. I like learning. Most of my knowledge comes from books, other humans and life experiences. I am fluent in English, Hebrew and Russian and currently learning

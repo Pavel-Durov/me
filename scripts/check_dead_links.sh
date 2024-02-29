6639f3435c2175f34213d590eb0c21daf9aeebaa
Pavel Durov <pavel@p3ld3v.dev>
2024-02-29 18:30:26 +0000

    chore: skip zenrows

--- scripts/check_dead_links.sh
+++ scripts/check_dead_links.sh
@@ -14,10 +14,17 @@ set -euo pipefail
 URLS=$(find "${TARGET_DIR}" -type f -exec bash -c 'cat "$1"' _ {} \; | grep -oE 'https?://[^[:space:]]+' | sed 's/,$//' | sed "s/'//g")
 
 USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+
 for url in $URLS; do
     echo "Checking ${url}"
     status=$(curl -A "${USER_AGENT}" -s -o /dev/null -w "%{http_code}\n" "${url}")
     echo "Status: ${status}"
+
+     if [[ $url == *"www.zenrows.com"* ]]; then
+        echo "Skipping URL: $url"
+        continue
+    fi
+    
     if [ $status -ge 400 ]; then
         echo "Dead link: ${url}"
         exit 1

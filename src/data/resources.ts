09f5e9cd0329df7a17c9a5ab89e1cb23e430c604
Pavel Durov <pavel@p3ld3v.dev>
2025-08-06 11:54:21 +0100

    Another resource.

--- src/data/resources.ts
+++ src/data/resources.ts
@@ -2,7 +2,13 @@ import { Resource } from 'model';
 
 export default [
   {
-    title: '',
+    title: 'USE THE INDEX, LUKE!',
+    url: 'https://use-the-index-luke.com/sql/partial-results/window-functions',
+    description: 'A Guide to database performance for developers',
+    tags: ['sql', 'performance', 'guide', 'pagination', 'indexes']
+  },
+  {
+    title: 'A Definition of Done for Architectural Decision Making',
     url: 'https://www.ozimmer.ch/practices/2020/05/22/ADDefinitionOfDone.html',
     description:
       'Definition of Done for architectural decisions to complement Agile focus on features by providing clear completion criteria for technical design choices. He identifies five key elements—Evidence, Criteria, Agreement, Documentation, and Realisation/Review plan—that ensure decisions are well-founded, evaluated against alternatives, socially validated, recorded, and scheduled for implementation and later reassessment.',

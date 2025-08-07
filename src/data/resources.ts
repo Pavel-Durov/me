8d21b2182fc10bf58290f0ffddd7ab065065c264
Pavel Durov <pavel@p3ld3v.dev>
2025-08-07 15:53:15 +0100

    Add resource. (#158)
    * Add resource.

--- src/data/resources.ts
+++ src/data/resources.ts
@@ -2,10 +2,23 @@ import { Resource } from 'model';
 
 export default [
   {
+    title: 'USE THE INDEX, LUKE!',
+    url: 'https://use-the-index-luke.com/sql/partial-results/window-functions',
+    description: 'A Guide to database performance for developers',
+    tags: ['sql', 'performance', 'guide', 'pagination', 'indexes']
+  },
+  {
+    title: 'A Definition of Done for Architectural Decision Making',
+    url: 'https://www.ozimmer.ch/practices/2020/05/22/ADDefinitionOfDone.html',
+    description:
+      'Definition of Done for architectural decisions to complement Agile focus on features by providing clear completion criteria for technical design choices. He identifies five key elements—Evidence, Criteria, Agreement, Documentation, and Realisation/Review plan—that ensure decisions are well-founded, evaluated against alternatives, socially validated, recorded, and scheduled for implementation and later reassessment.',
+    tags: ['architecture', 'decision-making', 'principles', 'technology radar', 'adr', 'agile']
+  },
+  {
     title: 'PromptPex: Automatic Test Generation for Language Model Prompts',
     url: 'https://arxiv.org/abs/2503.05070',
     description:
-      'This paper presents PromptPex, a tool for automatically generating unit tests for prompts used in software applications with large language models (LLMs). artifacts, PromptPex extracts structured input/output specifications from prompts and uses them to create test cases. It evaluates prompt robustness across different LLMs by identifying when outputs fail to comply with prompt-defined constraints.',
+      'This paper presents PromptPex, a tool for automatically generating unit tests for prompts used in software applications wzith large language models (LLMs). artifacts, PromptPex extracts structured input/output specifications from prompts and uses them to create test cases. It evaluates prompt robustness across different LLMs by identifying when outputs fail to comply with prompt-defined constraints.',
     tags: [
       'prompt engineering',
       'test generation',
